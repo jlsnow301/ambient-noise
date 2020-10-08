b8e5d184e770aa126d6a9f591b94b1480c3b5606
YAG0731 <57081128+YAG0731@users.noreply.github.com>
2020-10-08 14:12:49 -0700

    Create NewScreen.js

---
+++ screens/NewScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Card from "../components/Card";
+import MainButton from "../components/MainButton";
+import BodyText from "../components/BodyText";
+import DefaultStyles from "../constants/default-styles";
+
+
+const NewScreen = () => {
+    return (
+      <View style={styles.screen}>
+        <Text></Text>
+      </View>
+    );
+  };
+
+
+
+export default NewScreen;
