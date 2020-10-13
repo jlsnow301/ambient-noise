7822934f7b2cfe24b75be71353cae8ddaf18761a
Phil Kim <philipkim206@gmail.com>
2020-10-13 13:34:07 -0700

    first commit for LoadScreen

--- App.js
+++ App.js
@@ -5,13 +5,14 @@ import * as Font from 'expo-font';
 import { enableScreens } from 'react-native-screens';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import LoadScreen from './screens/LoadScreen';
 enableScreens();
 
 import Header from './components/Header';
 import BodyText from './components/BodyText';
 import LoginScreen from './screens/LoginScreen';
 import SavedScreen from './screens/SavedScreen';
-import NewScreen from './screens/NewScreen';
+import RecentlyAddedScreen from './screens/RecentlyAddedScreen';
 import HomeScreen from './screens/HomeScreen';
 
 const Stack = createStackNavigator();
@@ -19,11 +20,12 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <View style={styles.screen}>
+      <LoadScreen/>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Newest" component={NewScreen} />
+          <Stack.Screen name="Newest" component={RecentlyAddedScreen} />
           <Stack.Screen name="Saved" component={SavedScreen} />
         </Stack.Navigator>
       </NavigationContainer>
---
+++ screens/LoadScreen.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+
+
+const LoadScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.imageContainer}>
+      <Image source={require("../assets/logo.png")} style={styles.image} resizeMode="cover" />
+      </View>
+    </View>
+  )
+}
+
+export default LoadScreen
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+})
+
+// rnfes
