bb97e8f91d0d76bdba18a601eb592f147ea526d0
Phil Kim <philipkim206@gmail.com>
2020-10-14 16:31:49 -0700

    add search bar and moved navbar to bottom

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SearchBar } from 'react-native-elements'
 
 import Card from "../components/Card";
 import MainButton from "../components/MainButton";
@@ -10,20 +11,39 @@ import DefaultStyles from "../constants/default-styles";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.screen}>
-      <Text></Text>
-      <Button
-        title="Go to Newest Homes"
-        onPress={() => navigation.navigate('Newest')}
-        />
-       <Button
-        title="Go to Saved Screen"
-        onPress={() => navigation.navigate('Saved')}
-        />
-        <Button
-        title="Go to Login Screen"
-        onPress={() => navigation.navigate('Login')}
-        />
+    <View style={{ flex: 1}}>
+
+      <View style={styles.geobar}>
+        <SearchBar placeholder="Type here..."/>
+        {/* https://reactnativeelements.com/docs/searchbar/ */}
+      </View>
+
+
+
+
+
+      <View style={styles.navbar}>
+        <Button style={styles.btnIcon}
+          title="Search"
+          onPress={() => navigation.navigate('Search')}
+          />
+        <Button style={styles.btnIcon}
+          title="Recent"
+          onPress={() => navigation.navigate('Recent')}
+          />
+        <Button style={styles.btnIcon}
+          title="Saved"
+          onPress={() => navigation.navigate('Saved')}
+          />
+        <Button style={styles.btnIcon}
+          title="Profile"
+          onPress={() => navigation.navigate('Profile')}
+          />
+          <Button style={styles.btnIcon}
+          title="More"
+          onPress={() => navigation.navigate('More')}
+          />
+      </View>
     </View>
   );
 };
@@ -36,4 +56,21 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: "center",
   },
+  navbar: {
+    padding: 5,
+    backgroundColor: "#d8ebff",
+    borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 15,
+    width: '100%'
+    
+  },
+  btnIcon: {
+    height: 25,
+    width: 25,
+    flex: 1,
+  }
 });
