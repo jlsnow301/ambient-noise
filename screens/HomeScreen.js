a0100ba87679d9e4a6db5cdbe79799147aad488a
Yunao Guo <57081128+YAG0731@users.noreply.github.com>
2020-10-15 15:27:51 -0700

    display record btn

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,8 +8,14 @@ import {
 } from "react-native";
 
 import { SearchBar } from "react-native-elements";
+import RecordButton from "../components/RecordButton";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import PlayButton from "../components/PlayButton";
 
-function HomeScreen() {
+
+
+function HomeScreen({ navigation }) {
   const [enteredValue, setEnteredValue] = useState("");
   const [confirmedSearch, setConfirmedSearch] = useState("");
 
@@ -45,15 +51,36 @@ function HomeScreen() {
           {/* MAP GOES HERE. JUST MAKE IT A COMPONENT*/}
           {/* <MapOverlay search={enteredValue}/> */}
           {/* props.search will be the user's submitted search.. I think... */}
+
+          <View style={styles.twobtcontainers}>
+            <PlayButton title="play the sound"
+              onPress={() => navigation.navigate('')}
+            />
+            <RecordButton title="go to modal screen"
+              onPress={() => navigation.navigate('MyModal')}
+            />
+          </View>
+
         </View>
+
+
+
       </View>
-    </TouchableWithoutFeedback>
+    </TouchableWithoutFeedback >
   );
 }
 
 export default HomeScreen;
 
+
 const styles = StyleSheet.create({
+  twobtcontainers: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
   buttonContainers: {
     flex: 1,
   },
