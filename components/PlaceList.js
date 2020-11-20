2aafe7ce1a267ba16d8b0e93a60558f746fe12af
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2020-11-19 18:20:05 -0800

    Integrated Firebase

--- components/PlaceList.js
+++ components/PlaceList.js
@@ -1,35 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import * as firebase from "firebase";
+import { StyleSheet, FlatList, TouchableOpacity, LogBox } from "react-native";
 
-import Location from "../models/location";
 import PlaceIcon from "./PlaceIcon";
 import PlaceItem from "./PlaceItem";
-
-import * as firebase from 'firebase';
-import { firebaseConfig } from "../services/firebase";
-import { LogBox } from 'react-native';
+import keys from "../constants/api-keys";
+import Location from "../models/location";
 
 // Testing only
 // import { LOCATIONS } from "../data/dummy-locations";
-
-
+// Initialize Firebase
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(keys.FIREBASE_CONFIG);
 }
 
-LogBox.ignoreLogs(['Setting a timer']);
-
-var LOCATIONS = [];
-
-const ref = firebase.database().ref('locations');
-
-ref.once('value', snapshot => {
-    snapshot.forEach(location=>{
-      LOCATIONS.push([new Location(location.val().id, location.val().coordinates, location.val().title, location.val().description, location.val().date, location.val().soundId)]);
-      console.log(LOCATIONS);
-    })
-})
-
+LogBox.ignoreLogs(["Setting a timer"]);
 
 const PlaceList = (props) => {
   const [placeData, setPlaceData] = useState({});
@@ -37,30 +22,46 @@ const PlaceList = (props) => {
 
   // Initially retrieves the database. Currently grabbing dummy data.
   useEffect(() => {
-    const getPlaceData = () => {
-      if (props.listMode === "saved") {
-        const savedData = LOCATIONS;
-        return savedData;
-      } else {
-        const recentData = LOCATIONS;
-        return recentData;
-      }
+    const getPlaceData = async () => {
+      let locations = [];
+      await firebase
+        .database()
+        // .ref(props.listMode) for when we have different dbs for recent/saved
+        .ref("locations")
+        .once("value", (snapshot) => {
+          snapshot.forEach((location) => {
+            locations.push(
+              new Location(
+                location.val().id,
+                location.val().coordinates,
+                location.val().title,
+                location.val().description,
+                location.val().date,
+                location.val().soundId
+              )
+            );
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      setPlaceData(locations);
     };
-    setPlaceData(getPlaceData);
+    getPlaceData();
   }, []);
 
   return (
-    <FlatList 
+    <FlatList
       keyExtractor={(placeData, item) => item.toString()}
       // keyExtractor={(item) => item.toString()}
       data={placeData}
       renderItem={(placeData) => (
-        <TouchableOpacity 
+        <TouchableOpacity
           onPress={() =>
             props.navigation.navigate("DetailsStack", placeData.item)
           }
         >
-          <PlaceItem  
+          <PlaceItem
             id={placeData.item.id}
             title={placeData.item.title}
             date={placeData.item.date}
