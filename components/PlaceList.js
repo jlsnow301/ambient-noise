21fe5a08e49890bc8e2f3b86e68e4b633d9e7b95
Sunny Lau <sunny.pslau@outlook.com>
2020-11-19 16:49:11 -0800

    get data from firebase

--- components/PlaceList.js
+++ components/PlaceList.js
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, FlatList, TouchableOpacity } from "react-native";
 
+import Location from "../models/location";
 import PlaceIcon from "./PlaceIcon";
 import PlaceItem from "./PlaceItem";
 
+import * as firebase from 'firebase';
+import { firebaseConfig } from "../services/firebase";
+import { LogBox } from 'react-native';
+
 // Testing only
-import { LOCATIONS } from "../data/dummy-locations";
+// import { LOCATIONS } from "../data/dummy-locations";
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+LogBox.ignoreLogs(['Setting a timer']);
+
+var LOCATIONS = [];
+
+const ref = firebase.database().ref('locations');
+
+ref.once('value', snapshot => {
+    snapshot.forEach(location=>{
+      LOCATIONS.push([new Location(location.val().id, location.val().coordinates, location.val().title, location.val().description, location.val().date, location.val().soundId)]);
+      console.log(LOCATIONS);
+    })
+})
+
 
 const PlaceList = (props) => {
   const [placeData, setPlaceData] = useState({});
@@ -26,16 +50,17 @@ const PlaceList = (props) => {
   }, []);
 
   return (
-    <FlatList
-      keyExtractor={(item) => item.id}
+    <FlatList 
+      keyExtractor={(placeData, item) => item.toString()}
+      // keyExtractor={(item) => item.toString()}
       data={placeData}
       renderItem={(placeData) => (
-        <TouchableOpacity
+        <TouchableOpacity 
           onPress={() =>
             props.navigation.navigate("DetailsStack", placeData.item)
           }
         >
-          <PlaceItem
+          <PlaceItem  
             id={placeData.item.id}
             title={placeData.item.title}
             date={placeData.item.date}
@@ -48,7 +73,6 @@ const PlaceList = (props) => {
     />
   );
 };
-
 export default PlaceList;
 
 const styles = StyleSheet.create({});
