2c122f7335b43ef374294561c755e7b56313024f
YAG0731 <57081128+YAG0731@users.noreply.github.com>
2020-11-23 13:13:41 -0800

    update

--- screens/MoreScreen.js
+++ screens/MoreScreen.js
@@ -3,35 +3,153 @@ import { Text, View, StyleSheet, TouchableOpacity, FadeInView } from 'react-nati
 import Constants from 'expo-constants';
 import RecordButton from "../components/RecordButton";
 import { Audio } from 'expo-av';
-import PlayButton from "../components/PlayButton"
 import { RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_DEFAULT } from 'expo-av/build/Audio';
-
-const recordSound = async () => {
-    Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        staysActiveInBackground: false,
-        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-        playsInSilentModeIOS: true,
-        shouldDuckAndroid: true,
-        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-        playThroughEarpieceAndroid: true
-      });
-    try {
-        const recording = new Audio.Recording();
-
-        let { canRecord, isDoneRecording } = await recording.getStatusAsync();
-        console.log({ canRecord, isDoneRecording });
-
-        let ready = await recording
-            .prepareToRecordAsync
-            ();
-        console.log({ ready });
-    } catch (error) {
-        console.log(`Error while recording ${error}`);
-    }
-};
+import StopButton from "../components/StopButton";
+import PlayBackButton from "../components/PlayBackButton";
+import PauseButton from "../componentss/PauseButton";
 
 const MoreScreen = (props) => {
+    const [isRecording, setIsRecording] = useState(false);
+    const [recording, setRecording] = useState(null);
+    const [sound, setSound] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        Audio.requestPermissionsAsync()
+    }, []);
+
+    const startRecording = async () => {
+
+        const { status } = Audio.getPermissionsAsync();
+        if (status !== 'granted') return;
+
+        // stop playback
+        if (sound !== null) {
+            await sound.unloadAsync();
+            sound.setOnPlaybackStatusUpdate(null);
+            setSound(null);
+        }
+
+        await Audio.setAudioModeAsync({
+            allowsRecordingIOS: true,
+            interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+            playsInSilentModeIOS: true,
+            shouldDuckAndroid: true,
+            interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+            playThroughEarpieceAndroid: false,
+            staysActiveInBackground: true,
+        });
+        const _recording = new Audio.Recording();
+        try {
+            await _recording.prepareToRecordAsync(recordingSettings);
+            setRecording(_recording);
+            await _recording.startAsync();
+            console.log("recording");
+            setIsRecording(true);
+        } catch (error) {
+            console.log("error while recording:", error);
+        }
+    };
+
+    const stopRecording = async () => {
+        try {
+            await recording.stopAndUnloadAsync();
+        } catch (error) {
+            // Do nothing -- we are already unloaded.
+        }
+        const info = await FileSystem.getInfoAsync(recording.getURI());
+        console.log(`FILE INFO: ${JSON.stringify(info)}`);
+        await Audio.setAudioModeAsync({
+            allowsRecordingIOS: false,
+            interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+            playsInSilentModeIOS: true,
+            playsInSilentLockedModeIOS: true,
+            shouldDuckAndroid: true,
+            interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+            playThroughEarpieceAndroid: false,
+            staysActiveInBackground: true,
+        });
+        const { sound: _sound, status } = await recording.createNewLoadedSoundAsync(
+            {
+                isLooping: true,
+                isMuted: false,
+                volume: 1.0,
+                rate: 1.0,
+                shouldCorrectPitch: true,
+            }
+        );
+        setSound(_sound);
+        setIsRecording(false);
+    };
+
+    // const uploadAudio = async () => {
+    //     const uri = recording.getURI();
+    //     try {
+    //         const blob = await new Promise((resolve, reject) => {
+    //             const xhr = new XMLHttpRequest();
+    //             xhr.onload = () => {
+    //                 try {
+    //                     resolve(xhr.response);
+    //                 } catch (error) {
+    //                     console.log("error:", error);
+    //                 }
+    //             };
+    //             xhr.onerror = (e) => {
+    //                 console.log(e);
+    //                 reject(new TypeError("Network request failed"));
+    //             };
+    //             xhr.responseType = "blob";
+    //             xhr.open("GET", uri, true);
+    //             xhr.send(null);
+    //         });
+    //         if (blob != null) {
+    //             const uriParts = uri.split(".");
+    //             const fileType = uriParts[uriParts.length - 1];
+    //             firebase
+    //                 .storage()
+    //                 .ref()
+    //                 .child(`nameOfTheFile.${fileType}`)
+    //                 .put(blob, {
+    //                     contentType: `audio/${fileType}`,
+    //                 })
+    //                 .then(() => {
+    //                     console.log("Sent!");
+    //                 })
+    //                 .catch((e) => console.log("error:", e));
+    //         } else {
+    //             console.log("erroor with blob");
+    //         }
+    //     } catch (error) {
+    //         console.log("error:", error);
+    //     }
+    // };
+
+    const downloadAudio = async () => {
+        const uri = await firebase
+            .storage()
+            .ref("nameOfTheFile.filetype")
+            .getDownloadURL();
+
+        console.log("uri:", uri);
+
+        // The rest of this plays the audio
+        const soundObject = new Audio.Sound();
+        try {
+            await soundObject.loadAsync({ uri });
+            await soundObject.playAsync();
+        } catch (error) {
+            console.log("error:", error);
+        }
+    };
+
+    const deleteRecordingFile = async () => {
+        try {
+            const info = await FileSystem.getInfoAsync(recording.getURI());
+            await FileSystem.deleteAsync(info.uri)
+        } catch(error) {
+            console.log("There was an error deleting recording file", error);
+        }
+    }
 
     return (
         <View style={styles.container}>
@@ -40,6 +158,14 @@ const MoreScreen = (props) => {
                 <RecordButton
                     onPress={() => { recordSound() }}
                 />
+                <StopButton
+                    onPress={() => { stopRecording() }}
+                />
+                <PlayBackButton
+                    onPress={() => { downloadAudio() }}
+                />
+
+                <PauseButton />
             </TouchableOpacity>
         </View>
     );
