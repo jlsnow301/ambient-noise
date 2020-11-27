e71563888abb27ec3aa66d64b2152f90f91c33ed
YAG0731 <57081128+YAG0731@users.noreply.github.com>
2020-11-26 22:56:06 -0800

    able to record

--- components/RecordButton.js
+++ components/RecordButton.js
@@ -4,32 +4,9 @@ import { FontAwesome5 } from "@expo/vector-icons";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 
 const RecordButton = (props) => {
-  const recordSound = async () => {
-    Audio.setAudioModeAsync({
-      allowsRecordingIOS: true,
-      staysActiveInBackground: false,
-      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-      playsInSilentModeIOS: true,
-      shouldDuckAndroid: true,
-      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-      playThroughEarpieceAndroid: true,
-    });
-    try {
-      const recording = new Audio.Recording();
-
-      let { canRecord, isDoneRecording } = await recording.getStatusAsync();
-      console.log({ canRecord, isDoneRecording });
-
-      let ready = await recording.prepareToRecordAsync();
-      console.log({ ready });
-    } catch (error) {
-      console.log(`Error while recording ${error}`);
-    }
-  };
-
   return (
     <View style={styles.container}>
-      <TouchableOpacity activeOpacity={0.8} onPress={() => recordSound()}>
+      <TouchableOpacity activeOpacity={0.8} onPress={props.onPress}>
         <View style={styles.button}>
           <FontAwesome5 name="microphone" size={40} color="#006AFF" />
         </View>
--- screens/MoreScreen.js
+++ screens/MoreScreen.js
@@ -2,68 +2,136 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity, FadeInView } from 'react-native';
 import Constants from 'expo-constants';
 import { Audio } from 'expo-av';
-import { RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_DEFAULT } from 'expo-av/build/Audio';
-
-import PlayButton from "../components/PlayButton"
+import { FontAwesome } from "@expo/vector-icons";
 import RecordButton from "../components/RecordButton";
 import SoundScore from "../components/SoundScore"
+import PlayBackButton from "../components/PlayBackButton";
+import StopButton from "../components/StopButton";
+import * as FileSystem from 'expo-file-system';
 
-const recordSound = async () => {
-    function storeSoundId(user, soundID) {
-        if (user != null) {
-          firebase
-            .database()
-            .ref('users/' + user.uid)
-            .set({
-              soundID: soundID,
-            });
-        }
-      }
-    Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        staysActiveInBackground: false,
-        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-        playsInSilentModeIOS: true,
-        shouldDuckAndroid: true,
-        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-        playThroughEarpieceAndroid: true
-      });
-    try {
+const MoreScreen = () => {
+    const [recording, setRecording] = useState(null);
+    const [isFetching, setIsFetching] = useState(false);
+    const [isRecording, setIsRecording] = useState(false);
+    const [sound, setSound] = useState(null);
+    const [isplaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        Audio.requestPermissionsAsync();
+    }, []);
+
+    const startRecording = async () => {
+
+        const { status } = await Audio.getPermissionsAsync();
+        if (status !== 'granted') return;
+
+        setIsRecording(true);
+        console.log('is recording: ' + isRecording);
+        await Audio.setAudioModeAsync({
+            allowsRecordingIOS: true,
+            interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+            playsInSilentModeIOS: true,
+            shouldDuckAndroid: true,
+            interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+            playThroughEarpieceAndroid: true,
+        });
         const recording = new Audio.Recording();
+        console.log('start Recording');
+        try {
+            await recording.prepareToRecordAsync();
+            await recording.startAsync();
+        } catch (error) {
+            console.log(error);
+            stopRecording();
+        }
 
-        let { canRecord, isDoneRecording } = await recording.getStatusAsync();
-        console.log({ canRecord, isDoneRecording });
+        setRecording(recording);
+    };
 
-        let ready = await recording
-            .prepareToRecordAsync
-            ();
-        console.log({ ready });
-    } catch (error) {
-        console.log(`Error while recording ${error}`);
+    const stopRecording = async () => {
+        setIsRecording(false);
+        try {
+            await recording.stopAndUnloadAsync();
+            console.log('recording stopped');
+        } catch (error) {
+        }
     }
-};
 
-const MoreScreen = (props) => {
+    const deleteRecordingFile = async () => {
+        try {
+            const info = await FileSystem.getInfoAsync(recording.getURI());
+            await FileSystem.deleteAsync(info.uri)
+        } catch (error) {
+            console.log("There was an error deleting recording file", error);
+        }
+    }
+
+    const resetRecording = () => {
+        deleteRecordingFile();
+        setRecording(null);
+    };
+
+    const getAudio = async () => {
+        setIsFetching(true);
+        try {
+            const info = await FileSystem.getInfoAsync(recording.getURI() || "");
+            console.log(`FILE INFO: ${JSON.stringify(info)}`);
+            setIsPlaying(true);
+            await Audio.setAudioModeAsync({
+              allowsRecordingIOS: false,
+              interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+              playsInSilentModeIOS: true,
+              shouldDuckAndroid: true,
+              interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+              playThroughEarpieceAndroid: false,
+              staysActiveInBackground: true,
+            });
+            const { sound, status } = await recording.createNewLoadedSoundAsync();
+            if(isplaying){
+                await sound.unloadAsync();
+            }
+            if(!isplaying){  
+                sound.playAsync();
+                setIsPlaying(false);
+            }         
+        } catch (error) {
+            console.log('There was an error reading file', error);
+            stopRecording();
+            resetRecording();
+        }
+        setIsFetching(false);
+    }
 
     return (
         <View style={styles.container}>
+            {isRecording &&
+                <FontAwesome name="microphone" size={32} color="#DE1C22" >recording...</FontAwesome>
+                
+            }
+            {!isRecording 
+            }
             <View>
-                <TouchableOpacity>
-                    <Text>Tap to record!</Text>
-                    <RecordButton
-                        onPress={() => { recordSound() }}
-                    />
-                </TouchableOpacity>
+                <RecordButton
+                    onPress={() => startRecording()}
+                />
+                <Text>Tap to record</Text>
+                <StopButton
+                    onPress={() => stopRecording()}
+                />
+                <Text>Tap to stop recording</Text>
+                <PlayBackButton
+                    onPress={() => getAudio()}
+                />
+                <Text>Tap to listen recorded audio</Text>
             </View>
             <View>
                 <TouchableOpacity>
-                    <SoundScore/>
+                    <SoundScore />
                 </TouchableOpacity>
             </View>
         </View>
     );
 }
-
 
 export default MoreScreen;
 
@@ -77,7 +145,7 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         position: 'absolute',
         alignItems: 'center',
-        top: '10%', left: 0, 
+        top: '10%', left: 0,
         right: 0, bottom: '30%',
 
     },
@@ -87,4 +155,13 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
     },
+
+    button: {
+        backgroundColor: '#48C9B0',
+        paddingVertical: 20,
+        width: '90%',
+        alignItems: 'center',
+        borderRadius: 5,
+        marginTop: 20,
+    }
 });
