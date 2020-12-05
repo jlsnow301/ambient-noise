90cdb9d009c5d47de19d3df4cc36df927ce950eb
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2020-12-04 22:48:58 -0800

    Object destructuring olympics

--- models/location.js
+++ models/location.js
@@ -1,11 +1,12 @@
 class Location {
-  constructor(id, coordinates, title, description, date, soundId) {
+  constructor(id, coordinates, title, description, date, soundId, ratings) {
     this.id = id;
     this.coordinates = coordinates;
     this.title = title;
     this.description = description;
     this.date = date;
     this.soundId = soundId;
+    this.ratings = ratings;
   }
 }
 
